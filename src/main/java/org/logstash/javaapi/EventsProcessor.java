7e7d60ab4583f0f490c990ddcad7224bd95b0e1d
axolotl <axolotl@172.20.10.3>
2019-04-26 13:04:50 +1000

    Added filtering of non-relevant spans

--- src/main/java/org/logstash/javaapi/EventsProcessor.java
+++ src/main/java/org/logstash/javaapi/EventsProcessor.java
@@ -25,8 +25,13 @@ public class EventsProcessor {
 		String id = event.getField("apm_id").toString();
 		Timestamp timestamp = (Timestamp) event.getField("apm_timestamp");
 
-		logger.debug("Received eventType=" + eventType + ", id=" + id + ", name=" + name + ", timestamp=" + timestamp.toEpochMilli());
+		logger.info("Received eventType=" + eventType + ", id=" + id + ", name=" + name + ", timestamp="
+				+ timestamp.toEpochMilli());
 
+		// Only allow empty stach for transaction starts
+		if (txStore.isEmpty() && !"TRANSACTION_START".equals(eventType))
+			return;
+		
 		if ("TRANSACTION_START".equals(eventType)) {
 			Transaction transaction = ElasticApm.startTransaction();
 			transaction.setName(name);
@@ -34,14 +39,39 @@ public class EventsProcessor {
 			transaction.setStartTimestamp(timestamp.toEpochMilli() * 1_000);
 			txStore.push(transaction);
 		} else if ("SPAN_START".equals(eventType)) {
+			
 			Span span = txStore.peek().startSpan();
 			span.setName(name);
 			createSpanTags(span, event);
 			span.setStartTimestamp(timestamp.toEpochMilli() * 1_000);
 			txStore.push(span);
-		} else if ("TRANSACTION_END".equals(eventType) || "SPAN_END".equals(eventType)) {
+		} else if ("EXCEPTION".equals(eventType)) {
+			
+			Span span = txStore.peek().startSpan();
+			span.setName(name);
+			createSpanTags(span, event);
+			span.setStartTimestamp(timestamp.toEpochMilli() * 1_000);
+			Throwable throwable = new RuntimeException(name);
+			span.captureException(throwable);
+			span.end(timestamp.toEpochMilli() * 1_000 + 1_000);
+			
+		} else if ("FLUSH".equals(eventType)) {
+
+			// End all spans
+			int size = txStore.size() - 1;
+			for (int i = 0; i < size; i++) {
+				txStore.pop().end(timestamp.toEpochMilli() * 1_000 + i * 1_000 + 1_000);
+			}
+			
+		} else if ("SPAN_END".equals(eventType)) {
 			txStore.pop().end(timestamp.toEpochMilli() * 1_000);
-		} 
+			
+		} else if ("TRANSACTION_END".equals(eventType)) {
+			int size = txStore.size();
+			for (int i = 0; i < size; i++) {
+				txStore.pop().end(timestamp.toEpochMilli() * 1_000 + i * 1_000 + 1_000);
+			}
+		}
 	}
 
 	private static void createSpanTags(Span span, Event event) {
